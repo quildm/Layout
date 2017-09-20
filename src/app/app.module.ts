44da067cd0ab1748b60853ba78955341faf6215e
Dennis Quilantang <quildm@ymail.com>
2017-09-19 17:47:09 -0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
+import { TaskListComponent } from './task/task-list/task-list.component';
+import { TaskNewComponent } from './task/task-new/task-new.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { MainComponent } from './main/main.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskComponent,
+    TaskListComponent,
+    TaskNewComponent,
+    HeaderComponent,
+    NavComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
